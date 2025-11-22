86188012bb8ecdb5ed119acfb42b3b538e2475df
yuheng <2513807550@qq.com>
2025-11-21 21:51:11 -0700

    initi database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
+import { prisma } from "@/lib/prisma";
 import React from "react";
 
-export default function Home() {
-  return <div className="font-bold">Hello world</div>;
+export default async function Home() {
+  const users = await prisma.user.findMany();
+
+  return <div className="font-bold">
+    {JSON.stringify(users, null, 2)}
+  </div>;
 }

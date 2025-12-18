1006ce8052123370dcae6a00a02eb1e0d0b47911
yuheng <2513807550@qq.com>
2025-12-17 22:53:01 -0700

    messages handling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,38 @@
-"use client"
+"use client";
 import { Button } from "@/components/ui/button";
 import { useTRPC } from "@/trpc/client";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 
 import React, { useState } from "react";
 import { toast } from "sonner";
 
 export default function Home() {
   const trpc = useTRPC();
-  const {isPending, mutate} = useMutation(trpc.greetin.invoke.mutationOptions({
-    onSuccess: () => {
-      toast.success("Background job started")
-    }
-  }));
+  const {data: messages} = useQuery(trpc.messages.getMany.queryOptions())
+  const createMessage = useMutation(
+    trpc.messages.create.mutationOptions({
+      onSuccess: () => {
+        toast.success("Message created");
+      },
+    })
+  );
   const [value, setValue] = useState("");
 
   return (
     <div className="font-bold ">
-      <input type="text" value={value} onChange={e => setValue(e.target.value)}></input>
-      <Button disabled={isPending} onClick={() => mutate({ value: value })}>invoke</Button>
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      ></input>
+      <Button
+        disabled={createMessage.isPending}
+        onClick={() => createMessage.mutate({ value: value })}
+      >
+        invoke
+      </Button>
+
+      {messages && JSON.stringify(messages)}
     </div>
   );
 }
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,7 +1,9 @@
+import { messagesRouter } from "@/modules/messages/server/procedures";
 import {  createTRPCRouter } from "../init";
 import { greetingRouter } from "./greeting";
 export const appRouter = createTRPCRouter({
-  greetin: greetingRouter
+  greetin: greetingRouter,
+  messages: messagesRouter
  
 });
 // export type definition of API
